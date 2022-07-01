ca213af4c4e2baf41fd14a5096b1fc4f83b24127
Kenan Yildirim <94038170+KYildirimds@users.noreply.github.com>
2022-07-01 15:25:57 +0200

    Add files via upload

--- capstone.py
+++ capstone.py
@@ -5,12 +5,16 @@ from PIL import Image
 import sklearn as sklearn
 import pickle
 
-image = Image.open("HR-Solution.jpg")
-st.image(image,width=500)
-st.title('Employee Churn Analysis')
+st.set_page_config(layout="wide")
 
-mlmodel = st.selectbox('Please select the maschine learning model',['Gradient Boosting','KNN','RandomForest','XGBoost'])
+image = Image.open("image.png")
+st.image(image,width=1040)
 
+st.markdown("<h1 style='text-align: center; color: black;'>Employee Churn Analysis</h1>", unsafe_allow_html=True)
+
+
+#st.title('Employee Churn Analysis')
+st.info('**We can make predictions according to the entered criterias with different machine learning models whether our employer will stay in our company or left our company.**')
 
 st.markdown("""
 <style>
@@ -20,13 +24,19 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+st.subheader('Please select the maschine learning model')
+
+mlmodel = st.selectbox(' ',['Gradient Boosting','KNN','RandomForest','XGBoost'])
+
+
+
 st.sidebar.markdown('**Criterias for Employee Churn Analysis**')
 
 satisfaction_level = st.sidebar.number_input('Satisfaction Level of Employer:',0.09,1.0,0.5,0.01)
 last_evaluation = st.sidebar.number_input('Last Evaluation of Employer:',0.36,1.0,0.5,0.01)
 number_project = st.sidebar.number_input('Number of Projects:',1,8,1,1)
 average_montly_hours = st.sidebar.number_input('Average Monthly Hours:',90,320,100,1)
-time_spend_company = st.sidebar.slider('Select a number for Workyear', min_value=1, max_value=11, value=5, step=1) 
+time_spend_company = st.sidebar.slider(' Time spend company(Workyear)', min_value=1, max_value=11, value=5, step=1) 
 work_accident = st.sidebar.radio('Work Accident',  ["Yes", "No"])
 promotion_last_5years = st.sidebar.radio('Promotion in last 5 years',  ["Yes", "No"])
 departments = st.sidebar.selectbox('Select Department',  ['sales', 'accounting', 'hr', 'technical', 'support', 'management',
@@ -58,6 +68,10 @@ columns=[
        'Departments_product_mng', 'Departments_sales', 'Departments_support',
        'Departments_technical']
 
+from IPython.core.display import HTML
+st.write(HTML(my_dict.iloc[:,:5].to_html(index=False, justify='left')))
+st.write(' ')
+st.write(HTML(my_dict.iloc[:,5:].to_html(index=False, justify='left')))
 
 codes = {'Low':0, 'Medium':1, 'High':2}
 codes1 = {'Yes':1, 'No':0}
@@ -68,8 +82,6 @@ my_dict['promotion_last_5years'] = my_dict['promotion_last_5years'].map(codes1)
 
 
 my_dict_dummy = pd.get_dummies(my_dict).reindex(columns=columns, fill_value=0)
-
-st.dataframe(my_dict)
 
 final_scaler = pickle.load(open('scaler.pkl', "rb"))
 
@@ -93,6 +105,8 @@ else:
     filename4 = "gradboosting.pkl"
     model = pickle.load(open(filename4, "rb"))
     pred = model.predict(my_dict_scaled)
+
+st.write(' ')
 
 if st.button('Predict'):
     if int(pred[0]) == 0:
